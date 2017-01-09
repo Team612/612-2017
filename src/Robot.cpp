87da98cc62ace1fb9517244259367fec9e8c857f
Mynameisspam1 <Mynameisspam1@gmail.com>
2017-01-09 15:58:35 -0500

    Changed to SampleRobot example because their command and iterative examples do not work

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,88 +1,98 @@
+#include <iostream>
 #include <memory>
+#include <string>
 
-#include <Commands/Command.h>
-#include <Commands/Scheduler.h>
-#include <IterativeRobot.h>
-#include <LiveWindow/LiveWindow.h>
+#include <Joystick.h>
+#include <SampleRobot.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
+#include <RobotDrive.h>
+#include <Timer.h>
 
-#include "Commands/ExampleCommand.h"
-#include "CommandBase.h"
+/**
+ * This is a demo program showing the use of the RobotDrive class.
+ * The SampleRobot class is the base of a robot application that will
+ * automatically call your Autonomous and OperatorControl methods at the right
+ * time as controlled by the switches on the driver station or the field
+ * controls.
+ *
+ * WARNING: While it may look like a good choice to use for your code if you're
+ * inexperienced, don't. Unless you know what you are doing, complex code will
+ * be much more difficult under this system. Use IterativeRobot or Command-Based
+ * instead if you're new.
+ */
+class Robot: public frc::SampleRobot {
+	frc::RobotDrive myRobot { 0, 1 }; // robot drive system
+	frc::Joystick stick { 0 }; // only joystick
+	frc::SendableChooser<std::string> chooser;
+	const std::string autoNameDefault = "Default";
+	const std::string autoNameCustom = "My Auto";
 
-class Robot: public frc::IterativeRobot {
 public:
-	void RobotInit() override {
-		chooser.AddDefault("Default Auto", new ExampleCommand());
-		// chooser.AddObject("My Auto", new MyAutoCommand());
+	Robot() {
+		//Note SmartDashboard is not initialized here, wait until RobotInit to make SmartDashboard calls
+		myRobot.SetExpiration(0.1);
+	}
+
+	void RobotInit() {
+		chooser.AddDefault(autoNameDefault, autoNameDefault);
+		chooser.AddObject(autoNameCustom, autoNameCustom);
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	void DisabledInit() override {
-
-	}
-
-	void DisabledPeriodic() override {
-		frc::Scheduler::GetInstance()->Run();
-	}
-
-	/**
+	/*
 	 * This autonomous (along with the chooser code above) shows how to select
 	 * between different autonomous modes using the dashboard. The sendable
 	 * chooser code works with the Java SmartDashboard. If you prefer the
 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString code to get the auto name from the text box below the Gyro.
+	 * GetString line to get the auto name from the text box below the Gyro.
 	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the if-else structure below with additional strings & commands.
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * if-else structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
-	void AutonomousInit() override {
-		/* std::string autoSelected = frc::SmartDashboard::GetString("Auto Selector", "Default");
-		if (autoSelected == "My Auto") {
-			autonomousCommand.reset(new MyAutoCommand());
-		}
-		else {
-			autonomousCommand.reset(new ExampleCommand());
-		} */
+	void Autonomous() {
+		auto autoSelected = chooser.GetSelected();
+		// std::string autoSelected = frc::SmartDashboard::GetString("Auto Selector", autoNameDefault);
+		std::cout << "Auto selected: " << autoSelected << std::endl;
 
-		autonomousCommand.reset(chooser.GetSelected());
-
-		if (autonomousCommand.get() != nullptr) {
-			autonomousCommand->Start();
-		}
-	}
-
-	void AutonomousPeriodic() override {
-		frc::Scheduler::GetInstance()->Run();
-	}
-
-	void TeleopInit() override {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != nullptr) {
-			autonomousCommand->Cancel();
+		if (autoSelected == autoNameCustom) {
+			// Custom Auto goes here
+			std::cout << "Running custom Autonomous" << std::endl;
+			myRobot.SetSafetyEnabled(false);
+			myRobot.Drive(-0.5, 1.0); // spin at half speed
+			frc::Wait(2.0);                // for 2 seconds
+			myRobot.Drive(0.0, 0.0);  // stop robot
+		} else {
+			// Default Auto goes here
+			std::cout << "Running default Autonomous" << std::endl;
+			myRobot.SetSafetyEnabled(false);
+			myRobot.Drive(-0.5, 0.0); // drive forwards half speed
+			frc::Wait(2.0);                // for 2 seconds
+			myRobot.Drive(0.0, 0.0);  // stop robot
 		}
 	}
 
-	void TeleopPeriodic() override {
-		frc::Scheduler::GetInstance()->Run();
+	/*
+	 * Runs the motors with arcade steering.
+	 */
+	void OperatorControl() override {
+		myRobot.SetSafetyEnabled(true);
+		while (IsOperatorControl() && IsEnabled()) {
+			// drive with arcade style (use right stick)
+			myRobot.ArcadeDrive(stick);
+
+			// wait for a motor update time
+			frc::Wait(0.005);
+		}
 	}
 
-	void TestPeriodic() override {
-		frc::LiveWindow::GetInstance()->Run();
-	}
+	/*
+	 * Runs during test mode
+	 */
+	void Test() override {
 
-private:
-	std::unique_ptr<frc::Command> autonomousCommand;
-	frc::SendableChooser<frc::Command*> chooser;
+	}
 };
 
 START_ROBOT_CLASS(Robot)
