6e9e186d0c1efdabc373d92e8a2a31055c7c456f
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-01-10 21:55:29 -0500

    Using David's working code to add built-in functionality for CTRE. Made .wpilib-download.sh actually check the version correctly, added a rm .build/cmake_install.cmake .build/CMakeCache.txt .build/FRCUserProgram .build/Makefile command

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-
 all:
 	sh build.sh
 
 deploy:
 	sh deploy.sh
-	
+
 update:
 	sh .wpilib-download.sh
-	
+
+clean:
+	rm .build/cmake_install.cmake .build/CMakeCache.txt .build/FRCUserProgram .build/Makefile
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,3 +1,5 @@
+//This is an example file and can be removed
+
 #include <iostream>
 #include <memory>
 #include <string>
@@ -8,90 +10,52 @@
 #include <SmartDashboard/SmartDashboard.h>
 #include <RobotDrive.h>
 #include <Timer.h>
+#include <CanTalonSRX.h>
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will
- * automatically call your Autonomous and OperatorControl methods at the right
- * time as controlled by the switches on the driver station or the field
- * controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
 class Robot: public frc::SampleRobot {
-	frc::RobotDrive myRobot { 0, 1 }; // robot drive system
-	frc::Joystick stick { 0 }; // only joystick
-	frc::SendableChooser<std::string> chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
+	frc::RobotDrive myRobot { 0, 1 };
+	frc::Joystick stick { 0 };
+    CanTalonSRX actuator { 0 };
 
 public:
 	Robot() {
-		//Note SmartDashboard is not initialized here, wait until RobotInit to make SmartDashboard calls
 		myRobot.SetExpiration(0.1);
 	}
 
 	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameCustom, autoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &chooser);
+		std::cout << "Robot enabled!" << std::endl;
 	}
 
-	/*
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	void Autonomous() {
-		auto autoSelected = chooser.GetSelected();
-		// std::string autoSelected = frc::SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
-
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-			std::cout << "Running custom Autonomous" << std::endl;
-			myRobot.SetSafetyEnabled(false);
-			myRobot.Drive(-0.5, 1.0); // spin at half speed
-			frc::Wait(2.0);                // for 2 seconds
-			myRobot.Drive(0.0, 0.0);  // stop robot
-		} else {
-			// Default Auto goes here
-			std::cout << "Running default Autonomous" << std::endl;
-			myRobot.SetSafetyEnabled(false);
-			myRobot.Drive(-0.5, 0.0); // drive forwards half speed
-			frc::Wait(2.0);                // for 2 seconds
-			myRobot.Drive(0.0, 0.0);  // stop robot
+		auto timer = std::make_shared<Timer>();
+		timer->Start();
+		while(!IsOperatorControl() && IsEnabled()) {
+			if(!(timer->Get() >= 10)) {
+				myRobot.ArcadeDrive(0.4f, 0);
+			} else {
+				myRobot.ArcadeDrive((double) 0, 0);
+				timer->Stop();
+			}
 		}
+		std::cout << "Cleaning autonomous assets" << std::endl;
 	}
 
-	/*
-	 * Runs the motors with arcade steering.
-	 */
 	void OperatorControl() override {
 		myRobot.SetSafetyEnabled(true);
 		while (IsOperatorControl() && IsEnabled()) {
-			// drive with arcade style (use right stick)
-			myRobot.ArcadeDrive(stick);
+			myRobot.TankDrive(stick.GetRawAxis(1), stick.GetRawAxis(5));
+            actuator.Set(stick.GetRawAxis(2));
 
-			// wait for a motor update time
 			frc::Wait(0.005);
 		}
 	}
 
-	/*
-	 * Runs during test mode
-	 */
 	void Test() override {
-
+		while(IsEnabled()) {
+			if(stick.GetRawButton(1)) {
+				std::cout << "Button 1 lol" << std::endl;
+			}
+		}
 	}
 };
 
