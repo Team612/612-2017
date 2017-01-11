51161479a6ca591bccef4db88a6a8c72ad428497
DavidFiumano <Mynameisspam1@gmail.com>
2017-01-11 10:31:18 -0500

    Added CTRE to the update function in the makefile it now unzips CTRE quietly (-q)

--- .get_ctre.sh
+++ .get_ctre.sh
@@ -2,7 +2,7 @@
 
 printf ".get_ctre: CTRE not found. Downloading..."
 wget -r -nd --progress=bar http://www.ctr-electronics.com//downloads/lib/CTRE_FRCLibs_NON-WINDOWS.zip
-unzip CTRE_FRCLibs_NON-WINDOWS.zip -d ctre_full
+unzip -q CTRE_FRCLibs_NON-WINDOWS.zip -d ctre_full
 printf ".get_ctre.sh: Cleaning up...\n"
 cp -r ctre_full/cpp CTRE
 rm -rf ctre_full
