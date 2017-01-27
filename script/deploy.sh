84d96eae3a4d3d0b956c97f7159c0f78858e0077
Alexbay218 <Alexbay218@gmail.com>
2017-01-27 09:57:57 -0500

    Update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -11,11 +11,11 @@ os=$(uname | tr '[:upper:]' '[:lower:]')
 
 if [ ${#team} -eq 4 ]; then
     team_ip="${team:0:2}.${team:2}"
-    printf "Team IP 10.$team_ip.20\n"
+    printf "Team IP 10.$team_ip.2\n"
     printf "Operating system: $os\n"
 elif [ ${#team} -eq 3 ]; then
     team_ip="${team:0:1}.${team:1}"
-    printf "Team IP 10.$team_ip.20\n"
+    printf "Team IP 10.$team_ip.2\n"
     printf "Operating system: $os\n"
 fi
 
@@ -31,9 +31,9 @@ if [[ ! "$os" == *"cygwin"* ]]; then
         ping -c 4 "172.22.11.2" ; static=$?
         if [ $static -ne 0 ] ; then
             echo "Failed to connect via static USB, falling back to guessing static ethernet..."
-            ping -c 4 "10.$team_ip.20" ; ethernet=$?
+            ping -c 4 "10.$team_ip.2" ; ethernet=$?
             if [ $ethernet -ne 0 ] ; then
-                echo "Connection failed on guess: 10.$team_ip.20!"
+                echo "Connection failed on guess: 10.$team_ip.2!"
                 echo "Connection to roboRIO failed on all points!"
             else
                 ssh "lvuser@10.$team_ip.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
@@ -57,9 +57,9 @@ else
         ping -n 4 "172.22.11.2" ; static=$?
         if [ $static -ne 0 ] ; then
             echo "Failed to connect via static USB, falling back to guessing static ethernet..."
-            ping -n 4 "10.$team_ip.20" ; ethernet=$?
+            ping -n 4 "10.$team_ip.2" ; ethernet=$?
             if [ $ethernet -ne 0 ] ; then
-                echo "Connection failed on guess: 10.$team_ip.20!"
+                echo "Connection failed on guess: 10.$team_ip.2!"
                 echo "Connection to roboRIO failed on all points!"
             else
                 ssh "lvuser@10.$team_ip.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
