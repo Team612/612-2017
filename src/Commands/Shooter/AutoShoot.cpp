55aeb747ae0012e8f4528f17d55ab9a3cce4eba1
Jeffrey-Wang <17jwapples@gmail.com>
2017-02-06 19:32:57 -0500

    bugfix AutoShoot

--- src/Commands/Shooter/AutoShoot.cpp
+++ src/Commands/Shooter/AutoShoot.cpp
@@ -8,28 +8,28 @@ AutoShoot::AutoShoot(): Command() {
 }
 
 // Called just before this Command runs the first time
-void Shoot::Initialize() {
+void AutoShoot::Initialize() {
 
 }
 
 // Called repeatedly when this Command is scheduled to run
-void Shoot::Execute() {
+void AutoShoot::Execute() {
 
 }
 
 // Make this return true when this Command no longer needs to run execute()
 // Proposed function: Shoot until the container is empty
-bool Shoot::IsFinished() {
+bool AutoShoot::IsFinished() {
     return false;
 }
 
 // Called once after isFinished returns true
-void Shoot::End() {
+void AutoShoot::End() {
 
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void Shoot::Interrupted() {
+void AutoShoot::Interrupted() {
 
 }
