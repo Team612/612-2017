1403fc5d5e552a7774621b35bd817b345ef227e3
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-02-17 11:27:36 -0500

    Actually implimented intake PID

--- src/Subsystems/Intake.cpp
+++ src/Subsystems/Intake.cpp
@@ -3,7 +3,11 @@
 Intake::Intake() : Subsystem("Intake") {
     RobotMap::intake_talon_right->SetControlMode(frc::CANSpeedController::ControlMode::kSpeed);
     RobotMap::intake_talon_right->SetInverted(true);
-    RobotMap::intake_talon_left->SetFeedbackDevice(CANTalon::FeedbackDevice::CtreMagEncoder_Relative);
+    RobotMap::intake_talon_right->SetFeedbackDevice(CANTalon::FeedbackDevice::CtreMagEncoder_Relative);
+    RobotMap::intake_talon_right->SetPID(frc::SmartDashboard::GetNumber("Intake P", 0.12),
+                                         frc::SmartDashboard::GetNumber("Intake I", 0.001),
+                                         frc::SmartDashboard::GetNumber("Intake D", 5),
+                                         frc::SmartDashboard::GetNumber("Intake ff", 0.08));
     RobotMap::intake_talon_left->SetControlMode(frc::CANSpeedController::ControlMode::kFollower);
     RobotMap::intake_talon_left->Set(RobotMap::intake_talon_right->GetDeviceID());
 }
