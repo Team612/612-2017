0ebb968afefc52092ad955d3ff414560d6a16ba8
Jeffrey-Wang <17jwapples@gmail.com>
2017-02-22 16:36:30 -0500

    Fix smoothcontroller; implement into OI and Drive

--- lib/lib612/SmoothController.cpp
+++ lib/lib612/SmoothController.cpp
@@ -1,5 +1,28 @@
 #include "SmoothController.h"
 namespace lib612 {
+SmoothController::SmoothController(int port) : XboxController(port) {	}
+
+
+
+	//x y trigger
+
+double SmoothController::GetSmoothX(frc::GenericHID::JoystickHand hand) {
+
+		return GetSmoothed(GetX(hand));
+
+}
+
+double SmoothController::GetSmoothY(frc::GenericHID::JoystickHand hand) {
+
+		return GetSmoothed(GetY(hand));
+
+}
+
+double SmoothController::GetSmoothTrigger(frc::GenericHID::JoystickHand hand) {
+
+		return GetSmoothed(GetTriggerAxis(hand));
+
+}
 double SmoothController::GetSmoothed(double x) {
 
 		if(x > 0)
@@ -10,5 +33,6 @@ double SmoothController::GetSmoothed(double x) {
 
 			return -((c * std::pow(x, 2)) + (b * abs(x)) + a); //reverse curve for negative values
 
-	}
+}
+
 }
--- lib/lib612/SmoothController.h
+++ lib/lib612/SmoothController.h
@@ -1,16 +1,23 @@
 #include <WPILib.h>
-
 namespace lib612 {
-class SmoothController : public XboxController {
+  class SmoothController : public XboxController {
 
-	//quadratic curve that makes full power around 80%, half power around 35%
-public:
-	double c = 0.2;
+  public:
+  	SmoothController(int port);
+  	double GetSmoothX(frc::GenericHID::JoystickHand hand);
+  	double GetSmoothY(frc::GenericHID::JoystickHand hand);
+  	double GetSmoothTrigger(frc::GenericHID::JoystickHand hand);
 
-	double b = 0.6;
+  private:
+  	//quadratic curve that makes full power around 80%, half power around 35%
 
-	double a = 0.0; //this should remain at 0 so that 0 is always 0
+  	double c = 0.2;
 
-	double GetSmoothed(double x);
-};
+  	double b = 0.6;
+
+  	double a = 0.0; //this should remain at 0 so that 0 is always 0
+
+  	double GetSmoothed(double x);
+
+  };
 }
