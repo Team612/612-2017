7d7cf1889a53622d0700dfb0f37475a41fb09de9
David Rowson <davidfr333@gmail.com>
2017-03-02 13:50:21 -0500

    fixed small bug

--- src/Commands/Test/IntakeTest.cpp
+++ src/Commands/Test/IntakeTest.cpp
@@ -12,24 +12,24 @@ void IntakeTest::Initialize() {
 void IntakeTest::Execute() {
   //CHANGE THE CHANNELS FOR THE GetCurrent METHODS TO THE REAL ONES
     if(RobotMap::pdp->GetCurrent(1) >= 35){
-        std::cout << "THE INTAKE MOTOR IS DRAWING WAY TO MUCH POWER // LEFT INTAKE MOTOR"<< set::endl;
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kRightRumble,1);
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kLeftRumble,1);
+        std::cout << "THE INTAKE MOTOR IS DRAWING WAY TO MUCH POWER // LEFT INTAKE MOTOR"<< std::endl;
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kRightRumble,1);
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kLeftRumble,1);
     }
     if(!(RobotMap::intake_talon_left->GetSetpoint() =< 0) && RobotMap::pdp->GetCurrent(1) == 0 ){
         std::cout <<"THE LEFT INTAKE MOTOR IS AT 0 AMPS AND THE SET POINT IS NOT 0 ";
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kRightRumble,1);
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kLeftRumble,1);
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kRightRumble,1);
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kLeftRumble,1);
       }
     if(RobotMap::pdp->GetCurrent(2) >= 35){
-        std::cout << "THE INTAKE MOTOR IS DRAWING WAY TO MUCH POWER // RIGHT INTAKE MOTOR" << set::endl;
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kRightRumble,1);
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kLeftRumble,1);
+        std::cout << "THE INTAKE MOTOR IS DRAWING WAY TO MUCH POWER // RIGHT INTAKE MOTOR" << std::endl;
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kRightRumble,1);
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kLeftRumble,1);
     }
     if(!(RobotMap::intake_talon_right->GetSetpoint() == 0) && RobotMap::pdp->GetCurrent(2) == 0  ){
         std::cout <<"THE RIGHT INTAKE MOTOR IS AT 0 AMPS AND THE SET POINT IS NOT 0 ";
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kRightRumble,1);
-        Robot::oi->getgunner()->setRumble(frc::GenericHID::kLeftRumble,1);
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kRightRumble,1);
+        Robot::oi->getgunner()->SetRumble(frc::GenericHID::kLeftRumble,1);
     }
 }
 
