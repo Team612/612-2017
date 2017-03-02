0b23e3832f24a8b430dc42bb19e4f0748188b8f9
David Rowson <davidfr333@gmail.com>
2017-03-02 14:39:32 -0500

    fixed stuff

--- src/Commands/Test/IntakeTest.cpp
+++ src/Commands/Test/IntakeTest.cpp
@@ -10,13 +10,13 @@ void IntakeTest::Initialize() {
 }
 
 void IntakeTest::Execute() {
-  //CHANGE THE CHANNELS FOR THE GetCurrent METHODS TO THE REAL ONES
+  //CHANGE THE CHANNELS FOR THE GetCurrent METHODS TO THE REAL CHANNLES ONES
     if(RobotMap::pdp->GetCurrent(1) >= 35){
         std::cout << "THE INTAKE MOTOR IS DRAWING WAY TO MUCH POWER // LEFT INTAKE MOTOR"<< std::endl;
         Robot::oi->getgunner()->SetRumble(frc::GenericHID::kRightRumble,1);
         Robot::oi->getgunner()->SetRumble(frc::GenericHID::kLeftRumble,1);
     }
-    if(!(RobotMap::intake_talon_left->GetSetpoint() =< 0) && RobotMap::pdp->GetCurrent(1) == 0 ){
+    if(!(RobotMap::intake_talon_left->GetSetpoint() == 0) && RobotMap::pdp->GetCurrent(1) == 0 ){
         std::cout <<"THE LEFT INTAKE MOTOR IS AT 0 AMPS AND THE SET POINT IS NOT 0 ";
         Robot::oi->getgunner()->SetRumble(frc::GenericHID::kRightRumble,1);
         Robot::oi->getgunner()->SetRumble(frc::GenericHID::kLeftRumble,1);
