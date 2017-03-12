61dede2145c1a4c29b1f80f8761da19990af080d
DavidFiumano <Mynameisspam1@gmail.com>
2017-03-12 12:08:18 -0400

    Opening commit

---
+++ lib/lib612/PIDAutoTuner.cpp
@@ -0,0 +1,1 @@
+#include "PIDAutoTuner.h"
---
+++ lib/lib612/PIDAutoTuner.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace lib612{
+  class PIDAutoTuner{
+  private:
+
+  protected:
+
+  public:
+    PIDAutoTuner();
+    ~PIDAutoTuner();
+  }
+}
