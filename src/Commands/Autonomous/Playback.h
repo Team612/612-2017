fed7a14960c4bd2abedd676188aa6592039191bc
Shaun Wu <Alexbay218@gmail.com>
2017-03-14 16:42:41 -0400

    Finshed FollowMP Playback

--- src/Commands/Autonomous/Playback.h
+++ src/Commands/Autonomous/Playback.h
@@ -7,6 +7,12 @@
 #include "../../RobotMap.h"
 #include <fstream>
 
+/*! \brief Class to read from file and playback
+	Reads from a file and executes through Talon SRX in RobotMap
+	Using std::fstream to read from file line by line and executes
+	according to the timemark.
+	Also uses voltage compensation mode to increase accuracy.
+*/
 class Playback: public Command {
 private:
     struct playback_frame {
@@ -17,6 +23,10 @@ private:
 
     bool isFinished;
 
+	/*! \brief Time counter variable
+		Used to check if the current timemark at a given 
+		line (in the file) is smaller than the current time.
+	*/
     uint32_t t = 0;
 
     playback_frame playback_frame_buf;
@@ -26,13 +36,29 @@ private:
     std::string filePath;
     std::ifstream inputFile;
 
+	/*! \brief Timer object
+		Used to check the current time in relation to 
+		the current timemark in the current line (in the file).
+	*/
     frc::Timer timer;
 
+	/*! \brief A multiplier variable
+		Used to playback at another speed. 
+		A multiplier of 0.5 will play at half the speed, 
+		while a multiplier of 2.0 will play at 2x speed of when it was recorded.
+		
+		Note: if the file was recorded with a max speed of 25%, it must be 
+		played back with a multiplier of 4 to achieve 100% 
+	*/
     const double TIME_MULTIPLIER = 1;
 
 public:
     Playback(std::string filePath);
     void Initialize() override;
+	/*! \brief Execution of MotionProfile
+		Executes at 60 times a second to drive the 
+		RobotMap Talon SRX according the file
+	*/
     void Execute() override;
     bool IsFinished() override;
     void End() override;
--- src/Commands/Motion Profiling/FollowMP.h
+++ src/Commands/Motion Profiling/FollowMP.h
@@ -3,27 +3,35 @@
 #include "lib612/MotionProfile.h"
 #include "Commands/Command.h"
 #include "../../Robot.h"
-//! A class to execute MotionProfile
-/*! \brief Execute MotionProfile
-	Takes a MotionProfile as a constructor parameter
-	Uses the Robot::drivetrain subsystem to follow MotionProfile
+/*! \brief A class to execute MotionProfile
+	Takes a MotionProfile as a constructor parameter.
+	Uses the Robot::drivetrain subsystem to follow MotionProfile.
 */
 class FollowMP: public Command {
 public:
     FollowMP(lib612::MotionProfile* mp);
 	
-	//! Time counter variable
-	/*! Used to scroll through the MotionProfile */
+	/*! \brief Time counter variable
+		Used to scroll through the MotionProfile 
+	*/
     unsigned int currentTimeMark;
-	//! Timer object
-	/*! Used to check the current time in relation to the MotionProfile.time vector */
+	
+	/*! \brief Timer object
+		Used to check the current time in relation to 
+		the MotionProfile.time vector.
+	*/
     frc::Timer timer;
-	//! MotionProfile object
-	/*! MotionProfile object used to access motion profile data */
+	
+	/*! \brief MotionProfile object
+		MotionProfile object used to access motion profile data 
+	*/
     lib612::MotionProfile* profile;
     virtual void Initialize();
-	//! Execution of MotionProfile
-	/*! Executes at 60 times a second to drive the Robot::drivetrain subsystem according the data */
+	
+	/*! \brief Execution of MotionProfile
+		Executes at 60 times a second to drive the 
+		Robot::drivetrain subsystem according the data.
+	*/
     virtual void Execute();
     virtual bool IsFinished();
     virtual void End();
