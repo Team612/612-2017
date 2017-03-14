a7edae8a92eb418fa17507f43c26264f034f9e7b
Jeffrey-Wang <17jwapples@gmail.com>
2017-03-14 17:05:32 -0400

    Documented Drive

--- src/Commands/Drive/Drive.cpp
+++ src/Commands/Drive/Drive.cpp
@@ -3,16 +3,17 @@
 #include <cmath>
 
 #define JOE
-
+/*! \brief Allows the driver to control the robot's drivetrain using the left and right sticks with a smoothed tank drive. */
+/*! Constructor for Drive. */
 Drive::Drive(): Command() {
     printf("Drive constructor\n");
     Requires(Robot::drivetrain.get());
 }
-
+/*! Intializes Drive. No fuction other than a printout notification. */
 void Drive::Initialize() {
     printf("Drive init\n");
 }
-
+/*! Takes joystick input and uses it to drive the drivetrain via HaloDrive or TankDrive, after dampening it to 60% power. */
 void Drive::Execute() {
     //exists to facilitate smoothing function testing
     //std::cout << "Drive.cpp: " << "Throttle: " << Robot::oi->getdriver()->GetSmoothY(frc::GenericHID::kLeftHand) << " Wheel: " << Robot::oi->getdriver()->GetSmoothX(frc::GenericHID::kRightHand) << std::endl;
@@ -27,16 +28,16 @@ void Drive::Execute() {
     //motor feed safety
     frc::Wait(0.005);
 }
-
+/*! Returns false. */
 bool Drive::IsFinished() {
     return false;
 }
-
+/*! Sets the drivetrain to stop. */
 void Drive::End() {
     printf("Info: Drive ended!\n");
     Robot::drivetrain->TankDrive(0, 0);
 }
-
+/*! Sets the drivetrain to stop. */
 void Drive::Interrupted() {
     printf("ERROR: Drive interrupted!\n");
     Robot::drivetrain->TankDrive(0, 0);
--- src/Commands/Drive/Drive.h
+++ src/Commands/Drive/Drive.h
@@ -2,15 +2,16 @@
 
 #include "Commands/Command.h"
 
+/*! \brief Allows the driver to control the robot's drivetrain using the left and right sticks with a smoothed tank drive. */
 class Drive: public Command {
 public:
-    Drive();
+    Drive(); /*! Constructor for Drive. */
 
-    virtual void Initialize();
-    virtual void Execute();
-    virtual bool IsFinished();
-    virtual void End();
-    virtual void Interrupted();
+    virtual void Initialize(); /*! Intializes Drive. No fuction other than a printout notification. */
+    virtual void Execute(); /*! Takes joystick input and uses it to drive the drivetrain via HaloDrive or TankDrive, after dampening it to 60% power. */
+    virtual bool IsFinished(); /*! Returns false. */
+    virtual void End(); /*! Sets the drivetrain to stop. */
+    virtual void Interrupted(); /*! Sets the drivetrain to stop. */
 
 private:
 
