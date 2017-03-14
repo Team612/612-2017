97aeff9fc888abd87d992db0033b84eb6698b8ca
david <Mynameisspam1@gmail.com>
2017-03-14 16:43:20 -0400

    Added Climber and Intake

--- src/Subsystems/Intake.h
+++ src/Subsystems/Intake.h
@@ -4,14 +4,35 @@
 #include "../RobotMap.h"
 #include "Commands/Subsystem.h"
 
+/*! \brief Intake subsystem class for the intake subsystem on the robot
+    Can Intake fuel, clear itself when jammed, and stop when required
+    Implementsa ramp rate to prevent browning out the robot
+*/
+
 class Intake: public frc::Subsystem {
 public:
+    /*! \brief Intake class constructor
+        Sets the talons on the intake subsystem to throttle mode (value between -1.0 and 1.0)
+        Sets the ramp rate on the talons
+        Sets one talon to follow mode
+    */
     Intake();
+    /*! \brief Initializes IntakeFuel as default command
+    */
     void InitDefaultCommand() override;
+    /*!
+        \brief Sets the Intake talons to full speed
+    */
     void IntakeFuel(); //intake with motor speed
+    /*! \brief Reverses the intake subsystem to clear balls when jammed
+    */
     void ClearBalls();
+    /*! \breif Stops the intake
+    */
     void Stop();
 private:
+    /*! Intake Speed constant, not currently used */
     const double INTAKE_SPEED = 1000;
+    /*!Ramp rate constant in max volts*/
     const double RAMP_RATE = 16                                                                                                               ;
 };
