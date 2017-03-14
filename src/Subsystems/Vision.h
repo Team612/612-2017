b21107eb271bf59c3dfaa42512b0b2cba336d2ef
ZachPerkins1 <thezperk@gmail.com>
2017-03-14 18:29:17 -0400

    Added documentation for vision things

--- src/Subsystems/Vision.h
+++ src/Subsystems/Vision.h
@@ -6,40 +6,104 @@
 #include <vector>
 #include "DigitalOutput.h"
 
+/*!
+ * @brief The class for the vision subsystem.
+ * This class stores and keeps track of all targets being detected on the vision microprocessor.
+ */
 class Vision: public Subsystem
 {
 private:
-	const std::string BOUNDING_KEY = "BOUNDING_COORDINATES";
-	const std::string IDS_KEY = "IDS";
+	const std::string BOUNDING_KEY = "BOUNDING_COORDINATES"; /// The key in the network table for the list of target coordinates
+	const std::string IDS_KEY = "IDS"; /// The key in the network table for the list of target IDs
 
-	std::shared_ptr<NetworkTable> table = NULL;
-	std::vector<std::shared_ptr<VisionTarget>> targets;
+	std::shared_ptr<NetworkTable> table = NULL; /// The network table used to transfer the target information
+	std::vector<std::shared_ptr<VisionTarget>> targets; /// A list of all targets being sent to the robot
 
-	std::shared_ptr<VisionTarget> goal;
+	std::shared_ptr<VisionTarget> goal; /// The current target that the robot is following
 
-	const float TARGET_ASPECT = 1.66f/1.00f;
+	const float TARGET_ASPECT = 1.66f/1.00f; /// The target aspect ratio for the goal's bounding box.
 
 public:
 	Vision();
 	void InitDefaultCommand();
+
+	/*!
+	 * @brief Pulls values from the vision network table
+	 * This function must be called periodically from Robot
+	 */
 	void PullValues();
 	//void SetCamera(const char* camera);
 
+	/*!
+	 * Lists all of the current targets detected by the camera.
+	 * @return A list of detected vision targets as VisionTarget objects
+	 */
 	std::vector<std::shared_ptr<VisionTarget>> GetAllTargets(); //List all detected vision targets
+
+	/*!
+	 * @brief Get the number of detected vision targets.
+	 * Basically equivalent to GetAllTargets().size()
+	 * @return The amount of detected targets
+	 */
 	int GetTargetAmount(); //Amount of detected vision targets (targets.size())
+
+	/*!
+	 * @brief Get a target by it's tracking ID.
+	 * @return The amount of detected targets
+	 */
 	std::shared_ptr<VisionTarget> GetTargetById(int id); //Get target by it's ID in our jank Python program
+
+	/*!
+	 * @brief Check to see if a target exists in the list of tracked targets
+	 * @param id The id of the target to check
+	 */
 	bool TargetExists(int id); //Check if an ID exists
+
+
+	/*!
+	 * @brief Check to see if a target exists in the list of tracked targets
+	 * @param target The target to check
+	 */
 	bool TargetExists(std::shared_ptr<VisionTarget> target);
 
 	/*
 	 * I am so sorry
 	 */
 
+	/*!
+	 * @brief Get the raw network table object.
+	 * @return The vision network table object.
+	 */
 	std::shared_ptr<NetworkTable> GetRawTable();
 
+	/*!
+	 * @brief Update which target the robot is aligning to.
+	 * @return Whether or not a target was found
+	 */
 	bool UpdateCurrentTarget();
+
+	/*!
+	 * @brief Get the current goal that the robot is trying to align to.
+	 * @return The tracked goal as a VisionTarget
+	 */
 	std::shared_ptr<VisionTarget> GetTrackedGoal();
-	void SetTrackedGoal(std::shared_ptr<VisionTarget>);
+
+	/*!
+	 * @brief Set which goal is being followed by the robot
+	 * Basically equivalent to SetTrackingID(target.getTrackingID())
+	 * @param target The target to track
+	 */
+	void SetTrackedGoal(std::shared_ptr<VisionTarget> target);
+
+	/*!
+	 * @brief Set the ID of the target the robot is following.
+	 * @param id The ID of the target to track
+	 */
 	void SetTrackingID(int id);
+
+	/*!
+	 * @brief Retrieve the ID of the target the robot is following.
+	 * @returb id The ID of the tracked target
+	 */
 	int GetTrackingID();
 };
--- src/VisionTarget.h
+++ src/VisionTarget.h
@@ -3,56 +3,121 @@
 #include "WPILib.h"
 #include <cmath>
 
+/// A struct to represent a fixed point.
 struct Point {
         int x;
         int y;
 };
 
-//Represents basically a bounding box (with a few perks)
+/*!
+* @brief A class to represent a tracked vision target.
+* Used to store information about each vision target being sent over. Basically a bounding box with some extra perks.
+*/
 class VisionTarget {
 public:
 
-    static const int PARAM_COUNT = 4;
+    static const int PARAM_COUNT = 4; /// The amount of parameters being sen
 
-    static const double HEIGHT;
-    static const double WIDTH;
+    static const double HEIGHT; /// The physical height of the target
+    static const double WIDTH; /// The physical width of the target
 
+    /*!
+     * @brief Uses a set of points and an ID to create a box
+     * @param initPoints The set of points
+     * @param id The target's ID
+     */
     VisionTarget(std::vector<int> initPoints, int id); //See "Set()"
     virtual ~VisionTarget();
 
-    Point GetUL(); //Upper-Left
+    /*!
+     * @brief Get the upper left point
+     * @return The upper left point
+     */
+    Point GetUL(); //
+    /*!
+     * @brief Get the lower left point
+     * @return The lower left point
+     */
     Point GetLL(); //Lower-Left
+    /*!
+     * @brief Get the upper right point
+     * @return The upper right point
+     */
     Point GetUR(); //Upper-Right
+    /*!
+     * @brief Get the lower right point
+     * @return The lower right point
+     */
     Point GetLR(); //Lower-Right
 
+    /*!
+     * @brief Get the center point
+     * @return The center point
+     */
     Point GetCenter();
 
+    /*!
+     * @brief Get width of the bounding box
+     * @return The width of the bounding box
+     */
     int GetWidth();
+
+    /*!
+     * @brief Get height of the bounding box
+     * @return The height of the bounding box
+     */
     int GetHeight();
 
     int GetHeightConvex();
+
+    /*!
+     * @brief Get the physical distance of the target from the wall
+     * @return The height of the bounding box
+     */
     double GetDistance();
 
+    /*!
+     * @brief Get the aspect ratio of the box
+     * @return Aspect ratio
+     */
     double GetAspectRatio(); //width/height
 
     int GetID(); //The tracking ID set by RoboRealm to distinguish one object as it moves frame to frame
 
-    void Set(std::vector<int> setPoints); //Sets the points in a size 8 vector in the following format {ulx, uly, urx, ury, llx, lly, lrx, lry}
-    //(This is hour RoboRealm formats points sent from bounding boxes)
+    /*!
+     * @brief Set the points of the box based on a vector.
+     * Sets the points in a size 8 vector in the following format {x, y, width, height}
+     * This is the way they are sent over from the microprocessor.
+     * @param setPoints The vector of points
+     */
+    void Set(std::vector<int> setPoints); 
 
+    /*!
+     * @brief Print the location and ID of the box for debugging
+     */
     void Print();
 
+    /*!
+     * @brief Find the box in a provided list that most closely matches the provided aspect ratio.
+     * @param aspect The target aspect ratio
+     * @param targets The list of targets
+     */
     static std::shared_ptr<VisionTarget> FindClosestAspect(double aspect, std::vector<std::shared_ptr<VisionTarget>> targets);
 
 private:
-    int x;
-    int y;
-    int width = 0;
-    int height = 0;
+    int x; /// X Coordinate
+    int y; /// Y Coordinate
+    int width = 0; /// Width
+    int height = 0; /// Height
     int hHeight;
 
-    int id; //The tracking ID as given by RoboRealm
+    int id; ///The tracking ID as given by the microprocessor
 
+    /*!
+     * @brief A simple function to ease the creation of points.
+     * @param x The x position
+     * @param y The y position
+     */
     Point MakePoint(int x, int y);
 
 };
