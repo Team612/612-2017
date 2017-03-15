2b9cff1ab1851112a685efc3a7e3e3b3a317c9ce
Jeffrey-Wang <17jwapples@gmail.com>
2017-03-15 17:13:12 -0400

    Document Climb.h

--- src/Commands/Climber/Climb.h
+++ src/Commands/Climber/Climb.h
@@ -2,16 +2,16 @@
 
 #include "Commands/Command.h"
 #include "../../Robot.h"
-
+/*! \brief Allows the gunner to control the climber with the right joystick. */
 class Climb: public Command {
 public:
-    Climb();
+    Climb(); /*! Climb constructor. No special function. */
 
-    virtual void Initialize();
-    virtual void Execute();
-    virtual bool IsFinished();
-    virtual void End();
-    virtual void Interrupted();
+    virtual void Initialize(); /*! Climb initializer. No special function. */
+    virtual void Execute(); /*! If the gunner's right joystick is pushed upwards, this will drive the climber with the strength at which the joystick is pushed. Else, the climber is stopped. */
+    virtual bool IsFinished(); /*! Returns false. */
+    virtual void End(); /*! Stops the climber. */
+    virtual void Interrupted(); /*! Stops the climber. */
 private:
-    const double CURRENT_THRESHOLD = 4; //TODO replace with a number that make sense
+    const double CURRENT_THRESHOLD = 4; //TODO remove or adjust and implement
 };
